dd35991a15d6c7bbe89e4f765868fd51ea782b9b
Mahrang Saeed <79680735+mahrang@users.noreply.github.com>
2026-09-01 19:47:02 -0700

    Update chatbot.h

--- src/chatbot.h
+++ src/chatbot.h
@@ -27,16 +27,10 @@ public:
     ChatBot(std::string filename); // constructor WITH memory allocation
     ~ChatBot(); // 1: destructor
 
-    //// STUDENT CODE
-    ////
-    
     ChatBot(const ChatBot &source); // 2: copy constructor
     ChatBot &operator=(const ChatBot &source); // 3: copy assignment operator
     ChatBot(ChatBot &&source); // 4: move constructor
     ChatBot &operator=(ChatBot &&source);  // 5: move assignment operator
-
-    ////
-    //// EOF STUDENT CODE
 
     // getters / setters
     void SetCurrentNode(GraphNode *node);
