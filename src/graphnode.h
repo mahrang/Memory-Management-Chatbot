86130891d406092ef6f0cc4646cf0686bde444a4
Mahrang Saeed <79680735+mahrang@users.noreply.github.com>
2026-09-01 20:17:10 -0700

    Update graphnode.h

--- src/graphnode.h
+++ src/graphnode.h
@@ -13,19 +13,12 @@ class GraphEdge;
 class GraphNode
 {
 private:
-    //// STUDENT CODE
-    ////
-/* Task 4:  All instances of GraphEdge are changed in a way such that each instance of GraphNode exclusively owns the outgoing GraphEdges and holds non-owning references to incoming GraphEdges. Appropriate smart pointers are used to do this.  */
     // data handles (owned)
-    // std::vector<GraphEdge *> _childEdges; original code
     std::vector<std::unique_ptr<GraphEdge>> _childEdges;  // edges to subsequent nodes
 
     // data handles (not owned)
     std::vector<GraphEdge *> _parentEdges; // edges to preceding nodes
     ChatBot _chatBot;
-
-    ////
-    //// EOF STUDENT CODE
 
     // proprietary members
     int _id;
@@ -34,7 +27,6 @@ private:
 public:
     // constructor / destructor
     GraphNode(int id);
-    //~GraphNode();  Task 0:  read comment in graphnode.cpp
 
     // getter / setter
     int GetID() { return _id; }
@@ -48,15 +40,7 @@ public:
     void AddEdgeToParentNode(GraphEdge *edge);
     void AddEdgeToChildNode(std::unique_ptr<GraphEdge> edge);
 
-    //// STUDENT CODE
-    ////
-
-    // void MoveChatbotHere(ChatBot *chatbot); original code
-    // related to task 5, line 42 in graphnode.cpp, & line 250 in chatlogic.cpp
     void MoveChatbotHere(ChatBot chatbot);
-
-    ////
-    //// EOF STUDENT CODE
 
     void MoveChatbotToNewNode(GraphNode *newNode);
 };
